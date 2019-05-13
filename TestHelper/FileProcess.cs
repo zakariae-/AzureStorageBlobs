f42bd0801053a68acf1b733f51e2f022b9b097e3
zakariae <zakariae@example.com>
2019-05-13 23:46:10 +0200

    Add library Helper

---
+++ TestHelper/FileProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace TestHelper
+{
+    public static class FileProcess
+    {
+        public static string GetLoadedAssemblyDirectory(Assembly loadedAssembly = null)
+        {
+            var binDirectory = $"{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}";
+            var loadedAssemblyLocation = (loadedAssembly ?? Assembly.GetCallingAssembly()).Location;
+
+            var indexOf = loadedAssemblyLocation.IndexOf(binDirectory, StringComparison.OrdinalIgnoreCase);
+            if (indexOf <= 0)
+                throw new Exception(
+                    $"Directory {binDirectory} not find in the assembly, " +
+                    	"you need to provide the loaded assembly !!");
+
+            return loadedAssemblyLocation.Substring(0, indexOf);
+        }
+    }
+}
